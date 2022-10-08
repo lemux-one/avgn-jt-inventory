db8dfc7d7da346e68fbf99e7ec9177113b159c11
LemuxOne <lemux.one@gmail.com>
2022-10-08 06:39:17 -0400

    bootstrap project using start.spring.io

---
+++ src/main/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplication.java
@@ -0,0 +1,13 @@
+package one.lemux.avgnjtinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvgnJtInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvgnJtInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package one.lemux.avgnjtinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvgnJtInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
