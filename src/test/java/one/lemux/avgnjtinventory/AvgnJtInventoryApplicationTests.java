42d62dc265abde9a97f0db12731f67471711a54c
LemuxOne <lemux.one@gmail.com>
2022-10-13 02:34:11 -0400

    add sanity checks to test effectively that the context is loading appropriately

--- src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
+++ src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
@@ -1,13 +1,37 @@
 package one.lemux.avgnjtinventory;
 
+import one.lemux.avgnjtinventory.repositories.ContainerRepository;
+import one.lemux.avgnjtinventory.repositories.ManagerRepository;
+import one.lemux.avgnjtinventory.repositories.ProductRepository;
+import one.lemux.avgnjtinventory.repositories.RoleRepository;
+import one.lemux.avgnjtinventory.repositories.SectionRepository;
+import one.lemux.avgnjtinventory.repositories.StoreRepository;
+import one.lemux.avgnjtinventory.repositories.TypeRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AvgnJtInventoryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired private ContainerRepository containers;
+    @Autowired private ManagerRepository managers;
+    @Autowired private ProductRepository products;
+    @Autowired private RoleRepository roles;
+    @Autowired private SectionRepository sections;
+    @Autowired private StoreRepository stores;
+    @Autowired private TypeRepository types;
+    
+    @Test
+    public void contextLoads() {
+        Assertions.assertThat(containers).isNotNull();
+        Assertions.assertThat(managers).isNotNull();
+        Assertions.assertThat(products).isNotNull();
+        Assertions.assertThat(roles).isNotNull();
+        Assertions.assertThat(sections).isNotNull();
+        Assertions.assertThat(stores).isNotNull();
+        Assertions.assertThat(types).isNotNull();
+    }
 
 }
