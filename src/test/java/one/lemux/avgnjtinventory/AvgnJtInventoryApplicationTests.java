c9ef5bc5dd3a63eaefcafe7a58b85da0f1329e90
LemuxOne <lemux.one@gmail.com>
2022-10-13 03:51:18 -0400

    add test to check health endpoint and some refactoring

--- src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
+++ src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
@@ -7,12 +7,18 @@ import one.lemux.avgnjtinventory.repositories.RoleRepository;
 import one.lemux.avgnjtinventory.repositories.SectionRepository;
 import one.lemux.avgnjtinventory.repositories.StoreRepository;
 import one.lemux.avgnjtinventory.repositories.TypeRepository;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AvgnJtInventoryApplicationTests {
 
     @Autowired private ContainerRepository containers;
@@ -23,15 +29,24 @@ class AvgnJtInventoryApplicationTests {
     @Autowired private StoreRepository stores;
     @Autowired private TypeRepository types;
     
+    @Autowired private MockMvc mvc;
+    
     @Test
     public void contextLoads() {
-        Assertions.assertThat(containers).isNotNull();
-        Assertions.assertThat(managers).isNotNull();
-        Assertions.assertThat(products).isNotNull();
-        Assertions.assertThat(roles).isNotNull();
-        Assertions.assertThat(sections).isNotNull();
-        Assertions.assertThat(stores).isNotNull();
-        Assertions.assertThat(types).isNotNull();
+        assertThat(containers).isNotNull();
+        assertThat(managers).isNotNull();
+        assertThat(products).isNotNull();
+        assertThat(roles).isNotNull();
+        assertThat(sections).isNotNull();
+        assertThat(stores).isNotNull();
+        assertThat(types).isNotNull();
     }
 
+    @Test
+    public void healthStatusIsUp() throws Exception {
+        mvc.perform(get(("/manage/health")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/vnd.spring-boot.actuator.v3+json"))
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
 }
