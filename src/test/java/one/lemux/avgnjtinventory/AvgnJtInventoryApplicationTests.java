399f5b62424bb8004a33274d56376fce67460deb
LemuxOne <lemux.one@gmail.com>
2022-10-13 04:04:59 -0400

    set environment for tests with a random port

--- src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
+++ src/test/java/one/lemux/avgnjtinventory/AvgnJtInventoryApplicationTests.java
@@ -16,7 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class AvgnJtInventoryApplicationTests {
 
